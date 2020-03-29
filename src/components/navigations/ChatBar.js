6b50f4371a0e617f21584dc0c77260514c72c4c5
Cagatay <cagatay.kapuagasi@gmail.com>
2020-03-29 20:15:20 +0300

    Add animation

--- src/components/navigations/ChatBar.js
+++ src/components/navigations/ChatBar.js
@@ -1,11 +1,10 @@
-import React, { useMemo } from 'react';
-import { View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
-import { ScaledSheet } from 'react-native-size-matters';
-import Icon from '../Icon';
-import { images, languages } from 'res';
+import React, { useMemo, useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Animated, SafeAreaView } from 'react-native';
+import { ScaledSheet, scale } from 'react-native-size-matters';
+import { Icon, Avatar } from '../';
+import { languages } from 'res';
 import { Actions } from 'react-native-router-flux';
 import { inject, observer } from 'mobx-react';
-import FastImage from 'react-native-fast-image';
 
 const { dropdown1, online, offline } = languages.t('chatBar');
 
@@ -18,15 +17,19 @@ const ChatBar = props => {
     },
   } = props;
 
+  const [translateY] = useState(new Animated.Value(0));
+  const [opacity] = useState(new Animated.Value(0));
   const data = [{ text: dropdown1, onPress: () => deleteRoom(id) }];
   const openDropdown = () => navStore.openDropDown(data, styles.dropdown);
-
-  const userStatus = useMemo(
-    () => (currentUserStatus !== null ? (currentUserStatus ? online : offline) : null),
-    [currentUserStatus]
+  const userStatus = useMemo(() => (currentUserStatus ? online : null), [currentUserStatus]);
+  useEffect(
+    () =>
+      Animated.parallel([
+        Animated.timing(translateY, { duration: 500, toValue: scale(userStatus ? -8 : 0) }),
+        Animated.timing(opacity, { duration: 500, toValue: userStatus ? 1 : 0 }),
+      ]).start(),
+    [userStatus]
   );
-
-  const source = profile_photo ? { uri: profile_photo } : images.user;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -34,10 +37,12 @@ const ChatBar = props => {
         <TouchableOpacity onPress={Actions.pop} style={styles.button}>
           <Icon type="antdesign" name="left" color="white" size={25} />
         </TouchableOpacity>
-        <FastImage source={source} style={styles.picture} />
-        <View>
-          <Text style={styles.name}>{username}</Text>
-          {userStatus && <Text style={styles.status}>{userStatus}</Text>}
+        <Avatar uri={profile_photo} style={styles.photo} />
+        <View style={styles.textContainer}>
+          <Animated.Text style={[styles.name, { transform: [{ translateY }] }]}>
+            {username}
+          </Animated.Text>
+          <Animated.Text style={[styles.status, { opacity }]}>{userStatus}</Animated.Text>
         </View>
       </View>
       <TouchableOpacity onPress={openDropdown}>
@@ -66,24 +71,29 @@ const styles = ScaledSheet.create({
     marginLeft: '5@s',
     paddingTop: '2@s',
   },
-  picture: {
+  photo: {
     height: '30@s',
     width: '30@s',
     borderRadius: '15@s',
     marginLeft: '5@s',
   },
   name: {
-    marginLeft: '10@s',
     color: 'white',
     fontWeight: 'bold',
     fontSize: '14@s',
+    position: 'absolute',
+  },
+  textContainer: {
+    height: '100%',
+    width: '60%',
+    marginLeft: '10@s',
+    justifyContent: 'center',
   },
   box1: {
     alignItems: 'center',
     flexDirection: 'row',
   },
   status: {
-    marginLeft: '10@s',
     color: 'white',
     fontWeight: '400',
     fontSize: '12@s',
