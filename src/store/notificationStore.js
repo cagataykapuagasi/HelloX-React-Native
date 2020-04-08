ee040df2497c558481dde7c7acf85c40fa8203df
Cagatay <cagatay.kapuagasi@gmail.com>
2020-04-08 20:00:57 +0300

    onNotification

--- src/store/notificationStore.js
+++ src/store/notificationStore.js
@@ -1,7 +1,7 @@
-import messaging from '@react-native-firebase/messaging';
 import { showMessage } from 'react-native-flash-message';
 import { User } from '../api';
 import { Actions } from 'react-native-router-flux';
+import firebase from 'react-native-firebase';
 
 export default class NotificationStore {
   constructor(store) {
@@ -9,23 +9,25 @@ export default class NotificationStore {
   }
 
   init = async () => {
-    const token = await messaging().getToken();
+    await firebase.messaging().requestPermission();
+    const token = await firebase.messaging().getToken();
+
     if (token) {
       User.postFCM(token);
     }
 
-    console.log('firebase :D', token);
+    firebase.notifications().onNotification(({ _data }) => {
+      const user = JSON.parse(_data.user);
 
-    messaging().onMessage(async ({ data: { user } }) => {
-      Actions.chat({ item: JSON.parse(user) });
-      console.log('test', JSON.parse(user));
-    });
+      if (this.store.chatStore.currentUser === user.username) {
+        return;
+      }
 
-    messaging().setBackgroundMessageHandler(async ({ data: { user } }) => {
-      setTimeout(() => {
-        Actions.chat({ item: JSON.parse(user) });
-      }, 1000);
-      console.warn('Message handled in the background!', JSON.parse(user));
+      showMessage({
+        message: {},
+        user,
+        onPress: () => Actions.chat({ item: user }),
+      });
     });
   };
 }
